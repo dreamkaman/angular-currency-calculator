d64433be5a0af2e98f7baac5098fd7826c3c9cdc
Dreamkaman <riabushko@gmail.com>
2023-07-24 18:03:28 +0300

    Next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { StoreModule } from '@ngrx/store';
-// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 
-// import { currenciesReducer } from './app.reducer';
-// import { environment } from 'src/environments/environment';
+import { currenciesReducer } from './app.reducer';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,11 +20,11 @@ import { HttpClientModule } from '@angular/common/http';
     CoreModule,
     PagesModule,
     HttpClientModule,
-    // StoreModule.forRoot(currenciesReducer),
-    // StoreDevtoolsModule.instrument({
-    //   maxAge: 25,
-    //   logOnly: environment.production
-    // }),
+    StoreModule.forRoot(currenciesReducer),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
